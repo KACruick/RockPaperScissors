fbc61e288e210e986066c319c93555e03b0c473d
Brandon Espinosa <espinbrandon49@gmail.com>
2024-04-19 16:19:39 -0400

    cleaned up console logs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gamePlay = document.getElementById("gamePlay")
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
-const scoreboard =document.getElementById("scoreboard")
+const scoreboard = document.getElementById("scoreboard")
 
 let countHuman = 0;
 let countComputer = 0;
@@ -18,47 +18,40 @@ function getRandomInt(max) { //randomly picks index number for array
     return Math.floor(Math.random() * max);
 }
 
-    //binding rock button (value) to our rock variable
-    rock.addEventListener("click", function(e) {
-        //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
-        selection = e.target.value
-        computerSelection = getComputerChoice();
-        //console.log(computerSelection)
-        playRound(selection, computerSelection)
-        //console.log("count human", countHuman)
-        //console.log("count computer: ", countComputer)
-        if(countHuman === 3 || countComputer === 3) {
-            console.log(countHuman)
-            console.log(countComputer)
-            endGame();
-        }
-    })
-    paper.addEventListener("click", function(e) {
-        //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
-        selection = e.target.value
-        computerSelection = getComputerChoice();
-        playRound(selection, computerSelection)
-        //console.log("count human", countHuman)
-        //console.log("count computer: ", countComputer)
-        if(countHuman === 3 || countComputer === 3) {
-            console.log(countHuman)
-            console.log(countComputer)
-            endGame();
-        }
-    })
-    scissors.addEventListener("click", function(e) {
-        //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
-        selection = e.target.value
-        computerSelection = getComputerChoice();
-        playRound(selection, computerSelection)
-       // console.log("count human", countHuman)
-        //console.log("count computer: ", countComputer)
-        if(countHuman === 3 || countComputer === 3) {
-            console.log(countHuman)
-            console.log(countComputer)
-            endGame();
-        }
-    })
+//binding rock button (value) to our rock variable
+rock.addEventListener("click", function (e) {
+    //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
+    selection = e.target.value
+    computerSelection = getComputerChoice();
+    playRound(selection, computerSelection)
+    console.log(selection, computerSelection)
+    console.log("count human", countHuman, "count computer: ", countComputer)
+    if (countHuman === 3 || countComputer === 3) {
+        endGame();
+    }
+})
+paper.addEventListener("click", function (e) {
+    //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
+    selection = e.target.value
+    computerSelection = getComputerChoice();
+    playRound(selection, computerSelection)
+    console.log(selection, computerSelection)
+    console.log("count human", countHuman, "count computer: ", countComputer)
+    if (countHuman === 3 || countComputer === 3) {
+        endGame();
+    }
+})
+scissors.addEventListener("click", function (e) {
+    //console.log(e.target.value) //e is event, target of the event(rock element), value is the value of that target(the attribute value of the element)
+    selection = e.target.value
+    computerSelection = getComputerChoice();
+    playRound(selection, computerSelection)
+    console.log(selection, computerSelection)
+    console.log("count human", countHuman, "count computer: ", countComputer)
+    if (countHuman === 3 || countComputer === 3) {
+        endGame();
+    }
+})
 
 
 function playRound(playerSelection, computerSelection) {
@@ -91,5 +84,7 @@ function playRound(playerSelection, computerSelection) {
 
 
 function endGame() {
+    console.log(countHuman)
+    console.log(countComputer)
     console.log("EndGame")
 }
